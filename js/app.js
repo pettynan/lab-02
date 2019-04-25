d0bb40faed0c99c31ce72377fb212ecd30be1297
Kent Ketter <kent.ketter1@gmail.com>
2019-04-25 10:00:10 -0700

    starting to paginate

--- js/app.js
+++ js/app.js
@@ -1,27 +1,42 @@
-
 let dataArray = [];
+let dataArray1 = [];
+let dataArray2 = [];
 let keywordArray = [];
 
-function ImageObj(description, horns, image_url, keyword, title) {
+function ImageObj(description, horns, image_url, keyword, title, herdNum) {
   this.description = description;
   this.horns = horns;
   this.image_url = image_url;
   this.keyword = keyword;
   this.title = title;
 
-  dataArray.push(this);
+  if (herdNum === 1) {
+    dataArray1.push(this);
+  } else dataArray2.push(this);
 }
 
 $.getJSON('../data/page-1.json', function (element) {
   for (let i = 0; i < element.length; i++) {
-    new ImageObj(element[i].description, element[i].horns, element[i].image_url, element[i].keyword, element[i].title);
+    new ImageObj(element[i].description, element[i].horns, element[i].image_url, element[i].keyword, element[i].title, 1);
   }
-  populateHTML();
+  console.log(dataArray1);
+  populateHTML(1);
   populateDropDown();
-
 });
 
-function populateHTML() {
+//load second json on page load
+$.getJSON('../data/page-2.json', function (element) {
+  for (let i = 0; i < element.length; i++) {
+    new ImageObj(element[i].description, element[i].horns, element[i].image_url, element[i].keyword, element[i].title, 2);
+  }
+  console.log(dataArray2);
+});
+
+function populateHTML(herdNum) {
+  if (herdNum === 1) {
+    dataArray = dataArray1;
+  } else {dataArray = dataArray2;}
+
   for (let i = 0; i < dataArray.length; i++) {
 
     //This will become handlebars
@@ -45,10 +60,17 @@ function populateDropDown() {
   }
 }
 
-$('#selector').on('change', selectorHandler);
+$('#selector').on('change', dropDownOnClickHandler);
 
-function selectorHandler(e) {
+function dropDownOnClickHandler(e) {
   $('img').hide();
   $(`.${e.target.options[e.target.selectedIndex].value}`).show();
-  console.log(e.target.options);
+}
+
+//radio event listeners herd1 + herd2
+$('#images').on('input', herdRadioHandler);
+// event handler to load
+function herdRadioHandler(e) {
+  $('#images').empty();
+  $().show();
 }
